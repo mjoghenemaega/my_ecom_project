c1291004a1c15a908c14434f21309d4dc4cb39ea
mjoghenemaega <mjoghenemaega@gmail.com>
2020-11-26 22:11:30 -0800

    added js funtionality and perfect checkout and cart page

--- src/Ecommerce/store/models.py
+++ src/Ecommerce/store/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
 	def __str__(self):
 		return self.name
 
+	
 	@property
 	def imageURL(self):
 		try:
@@ -36,15 +37,53 @@ class Order(models.Model):
 	complete = models.BooleanField(default=False, null=True, blank=False)
 	transaction_id = models.CharField(max_length=200, null=True)
 
+	
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def get_cart_total(self):
+	    orderitems = self.orderitem_set.all()
+	    total = sum([item.get_total for item in orderitems])
+	    return total
+
+	@property
+	def shipping(self):
+	    shipping = False
+	    orderitems = self.orderitem_set.all()
+	    for i in orderitems:
+	        if i.product.digital == False:
+	            shipping = True
+	    return shipping
+	    
+	@property
+
+	def get_cart_items(self):
+	    orderitems = self.orderitem_set.all()
+	    total = sum([item.quantity for item in orderitems])
+	    return total
+
 
 class OrderItem(models.Model):
-	Product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
+
+	@property
+	def shipping(self):
+	    shipping = False
+	    orderitems = self.orderitem_set.all()
+	    for i in orderitems:
+	        if i.product.digital == False:
+	            shipping = True
+	    return shipping	
+
 
 
 class ShippingAddress(models.Model):
--- src/Ecommerce/store/views.py
+++ src/Ecommerce/store/views.py
@@ -1,15 +1,70 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 # Create your views here.
+
+from .utils import cookieCart, cartData 
+
+# Create your views here.
+
 def store(request):
-	products = Product.objects.all()
-	context ={'products': products}
-	return render(request, 'store/store.html', context)
+
+     data = cartData(request)
+     cartItems = data['cartItems']
+
+     products = Product.objects.all()
+     context = {'products':products,"cartItems":cartItems}
+     return render(request, 'store/store.html', context)
 
 def cart(request):
-	context ={}
-	return render(request, 'store/cart.html', context)
+
+     data = cartData(request)
+     cartItems = data['cartItems']
+     order = data['order']
+     items = data['items']
+
+
+     context = {'items':items,'order':order,"cartItems":cartItems}
+     return render(request, 'store/cart.html', context)
+
+
 
 def checkout(request):
-	context ={}
-	return render(request, 'store/checkout.html', context)		
+
+     data = cartData(request)
+     cartItems = data['cartItems']
+     order = data['order']
+     items = data['items']
+          
+     context = {'items':items,'order':order,"cartItems":cartItems}
+     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+
+	print("productId:",productId)
+	print("Action:",action)
+
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order,created = Order.objects.get_or_create(customer=customer,complete=False)
+
+	orderItem,created = OrderItem.objects.get_or_create(order=order,product=product)
+	if action == "add":
+	      orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+	      orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <=0:
+	      orderItem.delete()
+
+	return JsonResponse('items were added', safe=False)	
+def process_Order(request):
+	return JsonResponse('payment complete',safe=False)	
